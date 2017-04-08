cce62566489aa0b8836a4425d5d97cb992a25983
Slyboots92 <michal.blach92@gmail.com>
2017-04-09 01:37:10 +0200

    terrain module started

---
+++ Assets/scripts/map/Cell.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public abstract class Cell  {
+
+    private Image m_Texture;
+    private float m_Resistance;
+    public Image Texture
+    {
+        get
+        {
+            return m_Texture;
+        }
+
+        set
+        {
+            m_Texture = value;
+        }
+    }
+
+    public float Resistance
+    {
+        get
+        {
+            return m_Resistance;
+        }
+
+        set
+        {
+            m_Resistance = value;
+        }
+    }
+}
---
+++ Assets/scripts/map/TerrainType.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  TerrainUtils  {
+
+    enum TerrainTypes
+    {
+        Grass,
+        Stone,
+    };
+
+
+	
+};
